91ba91e0330260b8aa0f413d7b9c5f5354f45a3e
mkhizes <lubanzi01trading@gmail.com>
2025-02-14 14:00:37 +0200

    edited files of the smarthub project

--- chamber/scripts/getting.js
+++ chamber/scripts/getting.js
@@ -1,4 +1,4 @@
-const form = document.querySelector('form');
+const form = document.querySelector('.contact-form');
         form.addEventListener('submit', (e) => {
             e.preventDefault();
             const formData = new FormData(form);
